bdb20185410d1ccc7945e56593f9a4b6c7863c82
Greg Brandon <gebbber@gmail.com>
2023-02-05 12:06:55 -0700

    Dimmed timestamps

--- src/log-event.js
+++ src/log-event.js
@@ -2,9 +2,9 @@ import timeStamp from './timestamp.js';
 import chalk from './chalk.js';
 
 export default function logEvent(...args) {
-    console.log(chalk.gray(timeStamp()), ...args);
+    console.log(chalk.dim(timeStamp()), ...args);
 }
 
 export function logError(...args) {
-    console.error(chalk.gray(timeStamp()), ...args);
+    console.error(chalk.dim(timeStamp()), ...args);
 }
