5170a903581ab1ffed965eca54db955aa9c3da8d
Greg Brandon <gebbber@gmail.com>
2023-02-05 11:51:33 -0700

    more color

--- src/timestamp.js
+++ src/timestamp.js
@@ -5,8 +5,8 @@ let lastDateTime = '';
 
 export default function timeStamp() {
     const reqTime = new Date();
-    const currDateTime = chalk.cyan('╒═════ ') + dateformat('yyyy-MM-dd hh:mm:ss (UTCO):', reqTime);
-    const milliseconds = chalk.cyan('├') + dateformat('SSSms', reqTime);
+    const currDateTime = chalk.cyan('╒═════ ' + dateformat('yyyy-MM-dd hh:mm:ss (UTCO):', reqTime));
+    const milliseconds = chalk.cyan('├' + dateformat('SSSms', reqTime));
 
     const parts = [];
     if (currDateTime !== lastDateTime) parts.push(currDateTime);
